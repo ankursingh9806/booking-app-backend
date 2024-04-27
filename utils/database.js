022cc81ae6081b03642d99c9c9d437c6220fb175
Ankur Singh <ankursingh9806@gmail.com>
2024-04-27 10:04:31 +0530

    create database.js

---
+++ utils/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("booking-app", "root", "#Aaa666aaa", {
+    dialect: "mysql",
+    host: "localhost",
+});
+
+module.exports = sequelize;
