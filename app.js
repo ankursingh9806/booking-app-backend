dd1bbf9c770bcc670f7d4bf86c0ebfe4369163ac
Ankur Singh <ankursingh9806@gmail.com>
2024-04-27 10:03:59 +0530

    create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const sequelize = require("./utils/database");
+const userRoute = require("./routes/userRoute");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(userRoute);
+
+sequelize
+    .sync()
+    .then((result) => {
+        app.listen(3000);
+        console.log("server is synced with database");
+    })
+    .catch((err) => {
+        console.error("server is unable to unable to sync with database:", err);
+    });
+sequelize.authenticate()
+    .then((result) => {
+        console.log("Node application is connected to database");
+    })
+    .catch((err) => {
+        console.error("Node application is unable to connect to database:", err);
+    });
